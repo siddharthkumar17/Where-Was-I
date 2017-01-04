569675ad683e3b94ee1bebce9e91cee717bb36fe
Daniel Zheng <daniel.zheng@emory.edu>
2017-01-04 20:27:44 +0000

    dynamic testing capabilities

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.get("/", function(req, res){
    res.send("Welcome to whereWasI, written by Sid Kumar and Daniel Zheng. The application is currently under development.");
 });
 
+app.get("/:tvshow", function(req, res){
+    var tvshow = req.params.tvshow;
+    var episode = Math.floor(1 + Math.random() * 100);
+    res.send("Testing dynamic capabilities. You are currently watching " + tvshow + " and are currently on episode number " + episode + ".");
+})
+
 //LISTENING PORT
 
 app.listen(process.env.PORT, process.env.IP, function(){
