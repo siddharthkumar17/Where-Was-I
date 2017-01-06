eeb78142b8d5f47f060515682907c6d2b9ba9e6e
Daniel Zheng <daniel.zheng@emory.edu>
2017-01-06 21:46:15 +0000

    update frontend

--- app.js
+++ app.js
@@ -3,20 +3,34 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 //ROUTE
 
 app.get("/", function(req, res){
-   res.send("Welcome to whereWasI, written by Sid Kumar and Daniel Zheng. The application is currently under development.");
+   res.render("home");
 });
 
-app.get("/:tvshow", function(req, res){
+app.get("/query=:query",function(req, res) {
+    var query = req.params.query;
+    res.send(query);
+});
+
+/*app.get("/:tvshow/:season/:episode", function(req, res){
     var tvshow = req.params.tvshow;
-    var episode = Math.floor(1 + Math.random() * 100);//LOL
-    res.send("Testing dynamic capabilities. You are currently watching " + tvshow + " and are currently on episode number " + episode + ".");
-})
+    var season = req.params.season;
+    
+    var episode = req.params.episode;
+    res.send("You are currently watching " + tvshow + " at season "+season+" episode " + episode + "."); //wait dont update this shit cus its useless were gonna be using EJS
+    
+    
+})*/
 
 //LISTENING PORT
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The server has started.");
 });
+
+//OK LETS USE MEVN SO VUE.JS IN PLACE OF ANGULAR, BECAUSE VUE IS A LOT EASIER TO LEARN AND SUITS OUR NEEDS VERY LIGHTWEIGHT ETC
---
+++ public/movie.js
@@ -0,0 +1,42 @@
+$('#selectShow').selectize({
+    valueField: 'title',
+    labelField: 'title',
+    searchField: 'title',
+    options: [],
+    create: false,
+    render: {
+        option: function(item, escape) {
+            var actors = [];
+            for (var i = 0, n = item.abridged_cast.length; i < n; i++) {
+                actors.push('<span>' + escape(item.abridged_cast[i].name) + '</span>');
+            }
+
+            return '<div>' +
+                '<img src="' + escape(item.posters.thumbnail) + '" alt="">' +
+                '<span class="title">' +
+                    '<span class="name">' + escape(item.title) + '</span>' +
+                '</span>' +
+                '<span class="description">' + escape(item.synopsis || 'No synopsis available at this time.') + '</span>' +
+                '<span class="actors">' + (actors.length ? 'Starring ' + actors.join(', ') : 'Actors unavailable') + '</span>' +
+            '</div>';
+        }
+    },
+    load: function(query, callback) {
+        if (!query.length) return callback();
+        $.ajax({
+            url: 'http://www.omdbapi.com/?',
+            type: 'GET',
+            dataType: 'jsonp',
+            data: {
+                q: query,
+                page_limit: 10,
+            },
+            error: function() {
+                callback();
+            },
+            success: function(res) {
+                callback(res.movies);
+            }
+        });
+    }
+});
